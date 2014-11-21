af4b079ba24a748c3acf183a5080c7b1cb8cf05b
Scisco <alireza@developmentseed.org>
2014-11-21 07:30:12 -0500

    Fixed static file serving with foreman

--- pillbox-engine/urls.py
+++ pillbox-engine/urls.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles import views
 
 import xadmin
 xadmin.autodiscover()
@@ -20,7 +20,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(xadmin.site.urls))
+    url(r'^', include(xadmin.site.urls)),
+    url(r'^static/(?P<path>.*)$', views.serve),
     # Your stuff: custom urls go here
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
