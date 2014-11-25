216f673668db152f5a9e891e303c9fb242a365d4
Scisco <alireza@developmentseed.org>
2014-11-25 18:36:11 -0500

    Pillbox widget added to dashboard

---
+++ pillbox-engine/pillbox/templatetags/pillbox_tags.py
@@ -0,0 +1,33 @@
+from django import template
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+from pillbox.models import PillBoxData
+
+register = template.Library()
+
+
+@register.inclusion_tag('pillbox-engine/tags/widgets.html')
+def pillbox_widgets():
+    pillbox_count = PillBoxData.objects.all().count()
+
+    boxes = [
+        {
+            'icon': 'fa-briefcase',
+            'name': 'Pillbox Data',
+            'count': pillbox_count,
+            'text': 'Import Data',
+            'link': '#',
+            'color': 'primary'
+        }]
+
+    return {'boxes': boxes}
+
+
+@register.simple_tag
+def pillbox_sync_time():
+    last = PillBoxData.objects.all().order_by('-updated_at')[:1]
+
+    try:
+        return naturaltime(last[0].updated_at)
+    except:
+        return 'N/A'
