db65082234a9e710f90ef7a0146769c0fc0d3d6a
Scisco <alireza@developmentseed.org>
2014-11-26 13:32:43 -0500

    fixed ajax url and added a break mechanism

--- pillbox-engine/static/js/app.js
+++ pillbox-engine/static/js/app.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
         var that = $(this);
         if (action == 'products') {
             var progress = setInterval(function() {
-                $.get('/spl/sync/products', function(data) {
+                $.get('/spl/sync/products/')
+                 .done(function(data) {
                     that.html(data.total);
                     if (data.total > 60000) {
                         clearInterval(progress);
                     }
-                });
+                 })
+                 .fail(function() {
+                    clearInterval(progress);
+                 });
             }, 1000);
         }
 
