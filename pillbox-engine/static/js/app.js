9b1c85318eb1fb4fba8919eba6e7aac511c47cd5
Scisco <alireza@developmentseed.org>
2014-12-02 13:38:32 -0500

    bug fixings for spl widgets

--- pillbox-engine/static/js/app.js
+++ pillbox-engine/static/js/app.js
@@ -3,20 +3,19 @@ $(document).ready(function() {
     $('.panel-footer').click(function() {
         var action = $(this).data('action');
         var that = $(this);
-        if (action == 'products') {
-            var progress = setInterval(function() {
-                $.get('/spl/sync/products/')
-                 .done(function(data) {
-                    that.html(data.total);
-                    if (data.total > 60000) {
-                        clearInterval(progress);
-                    }
-                 })
-                 .fail(function() {
-                    clearInterval(progress);
-                 });
-            }, 1000);
-        }
-
+        $.get('/spl/sync/' + action + '/')
+         .done(function(data) {
+            if (data.status == 'PROGRESS') {
+                that.find('#panel-msg').html('Task Running: ' + data.total_processed + ' processed.');
+                that.children('.progress').removeClass('hide');
+                that.find('.progress-bar').css('width', data.percent + '%').html(data.percent + '%')
+            }
+            else {
+                that.find('#panel-msg').html(data.message);
+            }
+         })
+         .fail(function(data){
+            that.find('#panel-msg').html(data.responseJSON.message);
+         });
     })
 });
