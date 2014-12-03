031ab726aa499139d6e2a9d08cb43dbd5932f876
Scisco <alireza@developmentseed.org>
2014-12-03 11:12:20 -0500

    Add total and percent

--- pillbox-engine/pillbox/views.py
+++ pillbox-engine/pillbox/views.py
@@ -3,7 +3,7 @@ from __future__ import division
 from celery.result import AsyncResult
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from pillbox.models import Import
+from pillbox.models import Import, PillBoxData
 
 
 class ImportStatus(viewsets.ViewSet):
@@ -16,11 +16,24 @@ class ImportStatus(viewsets.ViewSet):
         if import_objs:
             for import_obj in import_objs:
                 task = AsyncResult(import_obj.task_id)
+                meta = task.info
+                if meta:
+                    try:
+                        processed = int(meta['added']) + int(meta['updated'])
+                    except TypeError:
+                        pass
+
+                total = PillBoxData.objects.all().count()
+                percent = round((processed / total) * 100, 2)
+
                 output = {
-                    'message': 'Importing...',
+                    'message': 'Importing',
                     'status': task.state,
                     'task_id': task.task_id,
-                    'meta': task.info
+                    'meta': meta,
+                    'total_processed': processed,
+                    'total': total,
+                    'percent': percent
                 }
         else:
             output = {
