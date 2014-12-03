703e0c7a44742e4529976504fa89b37a0742a74f
Scisco <alireza@developmentseed.org>
2014-12-03 11:56:45 -0500

    Improved pillbox data list view

--- pillbox-engine/pillbox/adminx.py
+++ pillbox-engine/pillbox/adminx.py
@@ -7,7 +7,7 @@ from pillbox.tasks import import_task
 
 class PillBoxDataAdmin(object):
 
-    list_display = ('medicine_name', 'setid', 'has_image')
+    list_display = ('medicine_name', 'source', 'author', 'has_image')
     # list_filter = ['product_code', 'dosage_form']
     list_quick_filter = ['splcolor', 'splsize', 'splscore']
     search_fields = ['medicine_name', 'part_medicine_name']
