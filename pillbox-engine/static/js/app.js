76cac996048d3d5ab697a029e693e8c2eb334e9c
Scisco <alireza@developmentseed.org>
2014-12-03 11:11:50 -0500

    Add refresh button + prevent anchor jump

--- pillbox-engine/static/js/app.js
+++ pillbox-engine/static/js/app.js
@@ -1,23 +1,40 @@
 
 $(document).ready(function() {
-    $('.panel-footer').click(function() {
+    $('.panel-footer').click(function(event) {
         if ($(this).parent().attr('href') == '#') {
+            event.preventDefault();
             var action = $(this).data('action');
             var that = $(this);
+
+            var addBar = function() {
+                that.children('.progress').removeClass('hide');
+            }
+
+            var removeBar = function() {
+                that.children('.progress').addClass('hide');
+            }
+
+            var changeButton = function(icon) {
+                that.find('.pull-right>i').attr('class', 'fa fa-' + icon);
+            }
+
+            changeButton('refresh');
+
             $.get(action)
              .done(function(data) {
                 if (data.status == 'PROGRESS') {
-                    that.find('#panel-msg').html('Task Running: ' + data.total_processed + ' processed.');
-                    that.children('.progress').removeClass('hide');
+                    that.find('#panel-msg').html(data.message + ': ' + data.total_processed + ' processed.');
+                    addBar();
+
                     that.find('.progress-bar').css('width', data.percent + '%').html(data.percent + '%')
                 }
                 else {
                     that.find('#panel-msg').html(data.message);
-                    that.children('.progress').addClass('hide');
+                    removeBar();
                 }
              })
              .fail(function(data){
-                that.children('.progress').addClass('hide');
+                removeBar();
                 that.find('#panel-msg').html(data.responseJSON.message);
              });
         }
