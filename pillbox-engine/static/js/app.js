a9ec19ef4e9a10329c4ec301e588a75a109b0585
Scisco <alireza@developmentseed.org>
2014-12-04 18:16:11 -0500

    Download widget added to dashboard

--- pillbox-engine/static/js/app.js
+++ pillbox-engine/static/js/app.js
@@ -1,42 +1,63 @@
 
 $(document).ready(function() {
-    $('.panel-footer').click(function(event) {
+
+    var addBar = function(elem) {
+        elem.children('.progress').removeClass('hide');
+    }
+
+    var removeBar = function(elem) {
+        elem.children('.progress').addClass('hide');
+    }
+
+    var changeButton = function(elem, icon) {
+        elem.find('.pull-right>i').attr('class', 'fa fa-' + icon);
+    }
+
+    $('#widget-footer').click(function(event) {
         if ($(this).parent().attr('href') == '#') {
             event.preventDefault();
             var action = $(this).data('action');
             var that = $(this);
-
-            var addBar = function() {
-                that.children('.progress').removeClass('hide');
-            }
-
-            var removeBar = function() {
-                that.children('.progress').addClass('hide');
-            }
-
-            var changeButton = function(icon) {
-                that.find('.pull-right>i').attr('class', 'fa fa-' + icon);
-            }
-
-            changeButton('refresh');
+            changeButton(that, 'refresh');
 
             $.get(action)
              .done(function(data) {
                 if (data.status == 'PROGRESS') {
                     that.find('#panel-msg').html(data.message + ': ' + data.total_processed + ' processed.');
-                    addBar();
+                    addBar(that);
 
                     that.find('.progress-bar').css('width', data.percent + '%').html(data.percent + '%')
                 }
                 else {
                     that.find('#panel-msg').html(data.message);
-                    removeBar();
+                    removeBar(that);
                 }
              })
              .fail(function(data){
-                removeBar();
+                removeBar(that);
                 that.find('#panel-msg').html(data.responseJSON.message);
              });
         }
     })
+
+    $('#download-widget-footer').click(function(event) {
+        event.preventDefault();
+        var action = $(this).data('action');
+        var that = $(this);
+        changeButton(that, 'refresh');
+        $.get(action)
+         .done(function(data) {
+            if (data.status == 'DOWNLOAD') {
+                that.find('#panel-msg').html(data.message);
+                addBar(that);
+
+                $('#overall').css('width', data.percent + '%').html(data.percent + '%')
+                $('#file-specific').css('width', data.meta.percent + '%').html(data.meta.percent + '%')
+            }
+            else {
+                that.find('#panel-msg').html(data.message);
+                removeBar(that);
+            }
+         });
+    });
 });
