3d38b007203673d2bee83d65a67c4a521e0d553e
Scisco <alireza@developmentseed.org>
2014-12-09 16:59:08 -0500

    Calculate size

--- pillbox-engine/spl/tasks.py
+++ pillbox-engine/spl/tasks.py
@@ -2,8 +2,13 @@ from __future__ import absolute_import, division
 import time
 import os
 import sys
+
 from django.utils import timezone
+from django.conf import settings
+
 from zipfile import BadZipfile
+from ftputil.error import TemporaryError
+
 from _celery import app
 from spl.sync.controller import Controller
 from spl.download import DownloadAndUnzip
@@ -30,17 +35,24 @@ def download_unzip(self, task_id, source_id):
 
     source = Source.objects.get(pk=source_id)
 
-    ## SET START STATUS
+    # SET START STATUS
     start = time.time()
     task = Task.objects.get(pk=task_id)
     task.status = 'STARTED'
     task.pid = os.getpid()
+    task.meta = {}
     task.save()
 
     # RUN THE TASK
     try:
-        dl = DownloadAndUnzip(task.id, source.title, source.files)
+        dl = DownloadAndUnzip(task.id, source.title, source.files,
+                              source.host, source.path)
         if dl.run():
+
+            # Calculate folder size for zip and unzip files
+            source.zip_size = folder_size(settings.DOWNLOAD_PATH + '/' + source.title)
+            source.unzip_size = folder_size(settings.SOURCE_PATH + '/' + source.title)
+            source.save()
 
             # SET END STATUS ON SUCCESS
             end = time.time()
@@ -56,11 +68,35 @@ def download_unzip(self, task_id, source_id):
             source.save()
 
     except BadZipfile:
-        end = time.time()
-        spent = end - start
-        task.status = 'FAILED'
-        task.is_active = False
-        task.time_ended = timezone.now()
-        task.duration = spent
-        task.traceback = sys.exc_info()
-        task.save()
+        record_error(task, start, sys.exc_info())
+        return
+    except TemporaryError as exc:
+        # Retry again if the connection timeout
+        raise self.retry(exc=exc)
+
+
+def folder_size(path):
+    """ Returns the folder size in Bytes of the give path """
+    total = 0
+    for dirpath, folders, filenames in os.walk(path):
+        for filename in filenames:
+            _file = os.path.join(dirpath, filename)
+            total += os.path.getsize(_file)
+            total = total
+    return total
+
+
+def record_error(task, start, traceback, status='FAILED'):
+    """ Updates the task model with a FAIL status
+    @param
+
+    """
+    end = time.time()
+    spent = end - start
+    task.status = status
+    task.is_active = False
+    task.time_ended = timezone.now()
+    task.duration = spent
+    task.traceback = traceback
+    task.save()
+
