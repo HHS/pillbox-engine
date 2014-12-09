e8940a2060bb3ad2faa9e6ae9a10d600bd4dc2dd
Scisco <alireza@developmentseed.org>
2014-12-09 16:58:50 -0500

    New size widget

--- pillbox-engine/spl/templatetags/spl_tags.py
+++ pillbox-engine/spl/templatetags/spl_tags.py
@@ -1,7 +1,9 @@
+import os
 from django import template
 from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.conf import settings
 
-from spl.models import SetInfo, ProductData, Ingredient, Download
+from spl.models import SetInfo, ProductData, Ingredient, Source, Task
 
 register = template.Library()
 
@@ -45,24 +47,69 @@ def spl_widgets():
 
 @register.inclusion_tag('pillbox-engine/tags/download_widgets.html')
 def download_widgets():
-    # download = Download.objects.filter(completed=True).order_by('-ended')[0:1].get()
-    download = Download.objects.all()[0:1].get()
-    box = {
-        'icon': 'fa-download',
-        'name': 'Download',
-        'time': download.started,
-        'text': 'Download New Data',
-        'link': '#',
-        'color': 'red',
-        'action': '/spl/download/',
-    }
-    return {'box': box}
+
+    sources = Source.objects.all().order_by('title')
+
+    colors = iter(['primary', 'green', 'yellow', 'red',
+                   'primary', 'green', 'yellow', 'red'])
+    boxes = []
+    for source in sources:
+
+        box = {
+            'icon': 'fa-download',
+            'name': source.title,
+            'text': 'Download New Data',
+            'link': '#',
+            'color': colors.next(),
+            'action': '/spl/download/%s/' % source.id,
+            'time': source.last_downloaded
+        }
+        try:
+            task = Task.objects.filter(is_active=True, download_type=source.title)[:1].get()
+            box['meta'] = task.meta
+            box['status'] = task.status
+        except Task.DoesNotExist:
+            pass
+
+        boxes.append(box)
+
+    return {'boxes': boxes}
+
+
+@register.inclusion_tag('pillbox-engine/tags/size_widgets.html')
+def size_widgets():
+
+    sources = Source.objects.all()
+
+    sunzip = 0
+    szip = 0
+
+    for s in sources:
+        sunzip += s.unzip_size
+        szip += s.zip_size
+
+    boxes = [
+        {
+            'size': round(szip / 1000000000, 2),
+            'path': settings.DOWNLOAD_PATH,
+            'name': 'Zip Folder Size',
+            'color': 'yellow'
+        },
+        {
+            'size': round(sunzip / 1000000000, 2),
+            'path': settings.SOURCE_PATH,
+            'name': 'Unzip Folder Size',
+            'color': 'red'
+        }
+    ]
+
+    return {'boxes': boxes}
 
 
 @register.simple_tag
 def spl_sync_time():
     last = ProductData.objects.all().order_by('-updated_at')[0:1].get()
-    time_since(last.updated_at)
+    return time_since(last.updated_at)
 
 
 def time_since(t):
