0c1a7f6bb69c1736c1f12dbd864f2ba16d49c368
Scisco <alireza@developmentseed.org>
2014-12-09 19:05:00 -0500

    unified api response

--- pillbox-engine/spl/views.py
+++ pillbox-engine/spl/views.py
@@ -3,7 +3,6 @@ import datetime
 
 from django.utils import timezone
 
-from celery.result import AsyncResult
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
@@ -51,6 +50,7 @@ class DownloadViewSet(viewsets.ViewSet):
 
                 # Start a new task
                 task = Task()
+                task.status = 'PENDING'
                 task.name = 'Download/Unzip'
                 task.download_type = source.title
                 task.time_started = timezone.now()
@@ -75,7 +75,7 @@ class SyncSpl(viewsets.ViewSet):
     """
 
     def list(self, request):
-        return Response({'message': 'It\'s working'})
+        return Response({'message': 'empty'}, status=status.HTTP_200_OK)
 
     @list_route()
     def pills(self, request):
@@ -96,62 +96,58 @@ class SyncSpl(viewsets.ViewSet):
             'products': SetInfo
         }
 
-        try:
-            job = Task.objects.filter(time_ended__exact=None)[0]
-        except IndexError:
-            job = None
+        if action in model_map:
+            try:
+                task = Task.objects.filter(is_active=True)[:1].get()
 
-        processed = 0
-
-        if action in model_map.keys():
-            if job:
-                if job.name != action:
-                    return Response({'message': 'Another task is running'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-
-                task = AsyncResult(job.task_id)
-                meta = task.info
-                if meta:
-                    try:
-                        processed = int(meta['added']) + int(meta['updated'])
-                    except TypeError:
-                        pass
-
-                total = model_map[action].objects.all().count()
-                percent = round((processed / total) * 100, 2)
-
-                if percent > 100:
-                    percent = 99
-
-                output = {
-                    'message': 'Syncing',
-                    'status': task.state,
-                    'task_id': task.task_id,
-                    'meta': meta,
-                    'total_processed': processed,
-                    'total': total,
-                    'percent': percent,
-                    'action': action
-                }
-            else:
-                jobs = Task.objects.filter(name=action,
-                                           time_ended__gte=datetime.datetime.today()-datetime.timedelta(days=1))
-                if jobs:
+                if task.name == action:
                     output = {
-                        'message': 'Sync Done. You can run the sync again in 24 hours.'
+                        'message': 'Syncing',
+                        'status': task.status,
+                        'task_id': task.task_id,
+                        'meta': task.meta,
+                        'pid': task.pid,
+                        'action': action
                     }
+
+                    return Response(output, status=status.HTTP_200_OK)
+
                 else:
-                    job = Task()
-                    job.name = action
-                    job.status = 'PENDING'
-                    job.save()
-                    sync = tasks.sync.delay(action, job.id)
-                    output = {
-                        'message': 'Process Started',
-                        'task_id': sync.task_id,
-                        'status': sync.state
-                    }
+                    return Response({'message': 'There is another task running.'},
+                                    status=status.HTTP_200_OK)
 
-            return Response(output, status=status.HTTP_200_OK)
+            except Task.DoesNotExist:
+
+                try:
+                    task = Task.objects.filter(
+                        name=action,
+                        time_ended__gte=datetime.datetime.today()-datetime.timedelta(days=1)
+                    )[:1].get()
+                    return Response({'message': 'Sync Done. You can run the sync again in 24 hours.'},
+                                    status=status.HTTP_200_OK)
+
+                except Task.DoesNotExist:
+
+                    task = Task()
+                    task.name = action
+                    task.status = 'PENDING'
+                    task.time_started = timezone.now()
+                    task.save()
+
+                    sync = tasks.sync.delay(action, task.id)
+
+                    task.task_id = sync.task_id
+                    task.save()
+
+                    return Response({
+                        'message': 'Sync started',
+                        'status': task.status,
+                        'meta': task.meta,
+                        'task_id': task.task_id,
+                        'pid': task.pid},
+                        status=status.HTTP_200_OK
+                    )
+
         else:
             return Response({'error': 'Action not supported'}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
