6155e8039b09cfdb926e32d9e2282aa828b05aa9
Scisco <alireza@developmentseed.org>
2014-12-12 14:51:25 -0500

    update pillbox upon verified

--- pillbox-engine/compare/models.py
+++ pillbox-engine/compare/models.py
@@ -25,11 +25,25 @@ class Color(CommonInfo):
     def __unicode__(self):
         return self.pillbox_value
 
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splcolor = self.pillbox_value
+            pill.save()
+        super(Color, self).save(*args, **kwargs)
+
 
 class Score(CommonInfo):
 
     def __unicode__(self):
         return self.pillbox_value
+
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splscore = self.pillbox_value
+            pill.save()
+        super(Score, self).save(*args, **kwargs)
 
 
 class Size(CommonInfo):
@@ -37,11 +51,25 @@ class Size(CommonInfo):
     def __unicode__(self):
         return self.pillbox_value
 
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splsize = self.pillbox_value
+            pill.save()
+        super(Size, self).save(*args, **kwargs)
+
 
 class Shape(CommonInfo):
 
     def __unicode__(self):
         return self.pillbox_value
+
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splshape = self.pillbox_value
+            pill.save()
+        super(Shape, self).save(*args, **kwargs)
 
 
 class Imprint(CommonInfo):
@@ -49,8 +77,22 @@ class Imprint(CommonInfo):
     def __unicode__(self):
         return self.pillbox_value
 
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splimprint = self.pillbox_value
+            pill.save()
+        super(Imprint, self).save(*args, **kwargs)
+
 
 class Image(CommonInfo):
 
     def __unicode__(self):
         return self.pillbox_value
+
+    def save(self, *args, **kwargs):
+        if getattr(self, 'verified', True):
+            pill = PillBoxData.objects.get(pk=self.pillbox_id)
+            pill.splimage = self.pillbox_value
+            pill.save()
+        super(Image, self).save(*args, **kwargs)
