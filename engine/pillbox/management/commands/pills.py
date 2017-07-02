9dedd2710a2d414ae7a5ace62186733634569d93
Scisco <scisco7@gmail.com>
2017-07-02 15:20:32 -0400

    add shell command for export

--- engine/pillbox/management/commands/pills.py
+++ engine/pillbox/management/commands/pills.py
@@ -2,11 +2,27 @@ from __future__ import print_function
 
 import os
 import time
+import traceback
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 
+from pillbox.models import Export
+from pillbox.exporter import export
 from spl.models import Task
 from compare.sync import transfer_new, compare
+
+
+def export_task(task_id, filename, export_type):
+    task = Task.objects.get(pk=task_id)
+    task.status = 'STARTED'
+    task.pid = os.getpid()
+    task.save()
+
+    export_file = export(filename, export_type, task_id)
+    task.refresh_from_db()
+    task.completed()
+
+    return export_file
 
 
 def transfer_task(task_id, action):
@@ -22,7 +38,7 @@ def transfer_task(task_id, action):
         print('this is a transfer action')
         transfer_new(task.id)
 
-    task = Task.objects.get(pk=task_id)
+    task.refresh_from_db()
     task.completed()
 
 
@@ -37,16 +53,38 @@ class Command(BaseCommand):
 
         if args and args[0] in arguments:
             try:
+                task = Task.create(args[0])
+                print('Starting the %s' % args[0])
+
                 if args[0] == 'transfer':
                     # create the task for import
-                    print('Starting the transfer')
-                    task = Task.create('transfer')
                     transfer_task(task.id, 'transfer')
 
                 elif args[0] == 'compare':
-                    print('Staring the compare')
-                    task = Task.create('compare')
                     transfer_task(task.id, 'compare')
+
+                elif args[0] == 'export':
+                    export = Export()
+                    export.file_type = 'json'
+                    export.file_name = 'pillbox_export.json'
+                    export.task_id = task.id
+                    export.save()
+
+                    try:
+                        export_file = export_task(task.id, export.file_name, export.file_type)
+                        task.refresh_from_db()
+                        export.export_file = export_file
+                    except Exception as e:
+                        task.refresh_from_db()
+                        task.cancelled('failed', e)
+                        traceback.print_exc()
+
+                    task.refresh_from_db()
+                    export.status = task.status
+                    export.duration = task.duration
+                    export.completed = True
+                    export.save()
+
 
             except KeyboardInterrupt:
                 task.refresh_from_db()
